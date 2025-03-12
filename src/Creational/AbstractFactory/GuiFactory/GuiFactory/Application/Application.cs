bb08449c9651c608e7e14db2403441900a370193
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 00:46:44 +0800

    feat: add implementation of dynamically changing GUI

---
+++ src/Creational/AbstractFactory/GuiFactory/GuiFactory/Application/Application.cs
@@ -0,0 +1,30 @@
+﻿using GuiFactory.Button;
+using GuiFactory.Checkbox;
+using GuiFactory.Gui;
+
+namespace GuiFactory.Application;
+
+public class Application
+{
+    private IGuiFactory Factory;
+    private IButton Button;
+    private ICheckbox Checkbox;
+
+    public Application(IGuiFactory NewFactory)
+    {
+        Factory = NewFactory;
+    }
+
+    public void CreateUI()
+    {
+        Button = Factory.CreateButton();
+        Checkbox = Factory.CreateCheckbox();
+    }
+
+    public void Paint()
+    {
+        Button.Paint();
+        Checkbox.Paint();
+    }
+
+}
---
+++ src/Creational/AbstractFactory/GuiFactory/GuiFactory/Program.cs
@@ -0,0 +1,16 @@
+﻿using GuiFactory.Application;
+using GuiFactory.Gui;
+
+Console.WriteLine("Choose GUI style (Windows/Mac): ");
+string style = Console.ReadLine();
+
+Application application;
+if (style == "Mac")
+    application = new Application(new MacFactory());
+else if (style == "Windows")
+    application = new Application(new WinFactory());
+else
+    throw new ArgumentException("Invalid GUI style.");
+
+application.CreateUI();
+application.Paint();
