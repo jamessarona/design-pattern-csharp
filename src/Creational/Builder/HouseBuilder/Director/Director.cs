0e57557ea25e7da4b15b1849b2237dc5117638d9
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 16:44:51 +0800

    feat: implement director

---
+++ src/Creational/Builder/HouseBuilder/Director/Director.cs
@@ -0,0 +1,33 @@
+﻿using HouseBuilder.Builder;
+
+namespace HouseBuilder.Director;
+
+public class Director
+{
+    private IBuilder _builder;
+
+    public Director(IBuilder builder)
+    {
+        _builder = builder;
+    }
+
+    public void ChangeBuilder(IBuilder builder)
+    {
+        _builder = builder;
+    }
+
+    public void Make(string type)
+    {
+        _builder.Reset();
+        if (type == "simple")
+        {
+            _builder.BuildStepA();
+        }
+        else
+        {
+            _builder.BuildStepB();
+            _builder.BuildStepZ();
+        }
+
+    }
+}
