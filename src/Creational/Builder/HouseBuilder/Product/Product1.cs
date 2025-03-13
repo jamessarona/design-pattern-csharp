46ec7da49f15e52a35bd3acd1989f68ec797d3b5
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 16:43:54 +0800

    feat: add product 1 and 2 class

---
+++ src/Creational/Builder/HouseBuilder/Product/Product1.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseBuilder.Product;
+
+public class Product1
+{
+    public string FeatureA { get; set; } = default!;
+    public string FeatureB { get; set; } = default!;
+    public string FeatureZ { get; set; } = default!;
+}
