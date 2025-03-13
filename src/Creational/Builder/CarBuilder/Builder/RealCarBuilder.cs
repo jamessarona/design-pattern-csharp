ee49878656961e3a0198f13e21c8580d6b615d89
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 17:42:31 +0800

    feat: implement real car builder

---
+++ src/Creational/Builder/CarBuilder/Builder/RealCarBuilder.cs
@@ -0,0 +1,43 @@
+﻿using CarBuilder.Model;
+
+namespace CarBuilder.Builder;
+
+public class RealCarBuilder : ICarBuilder
+{
+    private Car _car;
+
+    public RealCarBuilder()
+    {
+        _car = new Car();
+    }
+
+    public void Reset()
+    {
+        _car = new Car();
+    }
+
+    public void SetEngine(string engine)
+    {
+        _car.Engine = engine;
+    }
+
+    public void setGps()
+    {
+        _car.HasGPS = true;
+    }
+
+    public void SetSeats(int number)
+    {
+        _car.Seats = number;
+    }
+
+    public void setTripComputer()
+    {
+        _car.HasTripComputer = true;
+    }
+
+    public Car GetResult()
+    {
+        return _car;
+    }
+}
