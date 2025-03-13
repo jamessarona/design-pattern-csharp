930dbda667f03d4778e3548b2dc0aa92684e0320
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 17:41:56 +0800

    feat: implement car builder interface

---
+++ src/Creational/Builder/CarBuilder/Builder/ICarBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBuilder.Builder;
+
+public interface ICarBuilder
+{
+    void Reset();
+    void SetSeats(int Bumber);
+    void SetEngine(string Engine);
+    void setTripComputer();
+    void setGps();
+}
