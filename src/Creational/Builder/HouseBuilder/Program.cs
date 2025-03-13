7fc4e4abc1b7601b1d38337723dfbef3756c75c1
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 16:45:11 +0800

    feat: implement builder pattern with House features

---
+++ src/Creational/Builder/HouseBuilder/Program.cs
@@ -0,0 +1,22 @@
+﻿using HouseBuilder.Builder;
+using HouseBuilder.Director;
+using HouseBuilder.Product;
+
+ConcreteBuilder1 builder1 = new ConcreteBuilder1();
+
+Director director = new Director(builder1);
+
+director.Make("simple");
+Product1 product1 = builder1.GetResult();
+
+Console.WriteLine($"Product1: A={product1.FeatureA}, B={product1.FeatureB}, Z={product1.FeatureZ}");
+
+
+// Change the builder
+ConcreteBuilder2 builder2 = new ConcreteBuilder2();
+director.ChangeBuilder(builder2);
+
+director.Make("other");
+Product2 product2 = builder2.GetResult();
+
+Console.WriteLine($"Product2: A={product2.FeatureA}, B={product1.FeatureB}, Z={product1.FeatureZ}");
