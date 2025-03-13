210060e6d7c1f506addb94d595f7cf6a508bb152
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 17:43:05 +0800

    feat: add builder director for sports car and SUV

---
+++ src/Creational/Builder/CarBuilder/Director/Director.cs
@@ -0,0 +1,23 @@
+﻿using CarBuilder.Builder;
+
+namespace CarBuilder.Director;
+
+public class Director
+{
+    public void MakeSportsCar(ICarBuilder builder)
+    {
+        builder.Reset();
+        builder.SetSeats(2);
+        builder.SetEngine("Sport Engine");
+        builder.setTripComputer();
+        builder.setGps();
+    }
+
+    public void MakeSuv(ICarBuilder builder)
+    {
+        builder.Reset();
+        builder.SetSeats(5);
+        builder.SetEngine("SUV Engine");
+        builder.setGps();
+    }
+}
