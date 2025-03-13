d6d213cef388dd0548b85fb12b73c5095910a078
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-13 17:42:14 +0800

    feat: unokenebt car nabyak builder

---
+++ src/Creational/Builder/CarBuilder/Builder/CarManualBuilder.cs
@@ -0,0 +1,43 @@
+﻿using CarBuilder.Model;
+
+namespace CarBuilder.Builder;
+
+public class CarManualBuilder : ICarBuilder
+{
+    private Manual _manual;
+
+    public CarManualBuilder()
+    {
+        _manual = new Manual();
+    }
+
+    public void Reset()
+    {
+        _manual = new Manual();
+    }
+
+    public void SetEngine(string engine)
+    {
+        _manual.EngineInstructions = $"Engine: {engine}";
+    }
+
+    public void setGps()
+    {
+        _manual.GPSInstructions = "GPS installed.";
+    }
+
+    public void SetSeats(int number)
+    {
+        _manual.SeatInstructions = $"Seats: {number}";
+    }
+
+    public void setTripComputer()
+    {
+        _manual.TripComputerInstructions = "Trip Computer installed.";
+    }
+
+    public Manual GetResult()
+    {
+        return _manual;
+    }
+}
