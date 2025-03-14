21e9acfebfa1a5419d822fe2fefab08e3e947684
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-14 11:30:12 +0800

    feat: implement concrete prototype with cloning

---
+++ src/Creational/SamplePrototype/ConcretePrototype.cs
@@ -0,0 +1,21 @@
+﻿namespace SamplePrototype;
+
+public class ConcretePrototype : IPrototype
+{
+    public string Field1 { get; set; }
+
+    public ConcretePrototype(string field1)
+    {
+        Field1 = field1;
+    }
+
+    // Copy Constructor for Cloning
+    public ConcretePrototype(ConcretePrototype prototype)
+    {
+        Field1 = prototype.Field1;
+    }
+    public IPrototype Clone()
+    {
+        return new ConcretePrototype(this);
+    }
+}
