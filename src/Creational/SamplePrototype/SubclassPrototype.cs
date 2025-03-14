c9a066e3044aca305f4c430b6cbc38952911c299
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-14 11:30:37 +0800

    feat: implement subclass protototype of concrete prototype

---
+++ src/Creational/SamplePrototype/SubclassPrototype.cs
@@ -0,0 +1,22 @@
+﻿namespace SamplePrototype;
+
+public class SubclassPrototype : ConcretePrototype
+{
+    public string Field2 { get; set; }
+
+    public SubclassPrototype(string field1, string field2) : base(field1)
+    {
+        Field2 = field2;
+    }
+
+    public SubclassPrototype(SubclassPrototype prototype) : base(prototype)
+    {
+        Field2 = prototype.Field2;
+    }
+
+    public IPrototype Clone()
+    {
+        return new SubclassPrototype(this);
+    }
+
+}
