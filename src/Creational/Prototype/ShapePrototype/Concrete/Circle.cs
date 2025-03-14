38a1214ba766b66ab7c02ee22bb378750c7bfa85
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-14 12:32:06 +0800

    feat: implement subclass circle

---
+++ src/Creational/Prototype/ShapePrototype/Concrete/Circle.cs
@@ -0,0 +1,25 @@
+﻿using ShapePrototype.Base;
+
+namespace ShapePrototype.Concrete;
+
+public class Circle : Shape
+{
+    public int Radius { get; set; }
+
+    public Circle()
+    {
+    }
+
+    public Circle(Circle source) : base(source)
+    {
+        if (source != null)
+        {
+            Radius = source.Radius;
+        }
+    }
+
+    public override Shape Clone()
+    {
+        return new Circle(this);
+    }
+}
