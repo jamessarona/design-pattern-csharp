fe551ad5c83d1267603cdfcdf8e24759da3191a3
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-14 12:31:52 +0800

    feat: implement shape base class

---
+++ src/Creational/Prototype/ShapePrototype/Base/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace ShapePrototype.Base;
+
+
+public abstract class Shape
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public string Color { get; set; }
+
+    public Shape() { }
+
+    public Shape(Shape source)
+    {
+        if (source != null)
+        {
+            X = source.X;
+            Y = source.Y;
+            Color = source.Color;
+        }
+    }
+
+    public abstract Shape Clone();
+}
