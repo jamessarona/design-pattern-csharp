d2b263c3fbb019709b20396b825574957e11739f
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-14 12:54:34 +0800

    feat: implement singleton in database class

---
+++ src/Creational/Singleton/DatabaseSettings/Database.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlTypes;
+
+namespace DatabaseSettings;
+
+public class Database
+{
+    private static Database? _instance;
+    private static readonly object _lock = new();
+
+    private Database()
+    {
+
+    }
+
+    public static Database GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock) // Ensures thread safety
+            {
+                if (_instance == null)
+                {
+                    _instance = new Database();
+                }
+            }
+        }
+        return _instance;
+    }
+
+    public void Query(string Sql)
+    {
+        Console.WriteLine($"Query to database using ({Sql}).");
+    }
+}
