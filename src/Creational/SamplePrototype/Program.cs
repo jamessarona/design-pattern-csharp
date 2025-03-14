644e770407edbfe696af9d6c1ac859662b5f99eb
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-14 11:30:56 +0800

    feat: create sample scenario

---
+++ src/Creational/SamplePrototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using SamplePrototype;
+
+// Create a Concrete Prototype
+ConcretePrototype original = new ConcretePrototype("Value1");
+ConcretePrototype copy = (ConcretePrototype)original.Clone();
+Console.WriteLine($"Original: {original.Field1}, Copy: {copy.Field1}");
+
+// Create a Subclass Prototype
+SubclassPrototype subclassOriginal = new SubclassPrototype("Value1", "Value2");
+SubclassPrototype subclassCopy = (SubclassPrototype)subclassOriginal.Clone();
+Console.WriteLine($"Original: {subclassOriginal.Field1}, {subclassOriginal.Field2}");
+Console.WriteLine($"Copy: {subclassCopy.Field1}, {subclassCopy.Field2}");
