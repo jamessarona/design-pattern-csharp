9e18d96d11807ca143f75e45d2746039d0ac6c7b
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-14 12:54:53 +0800

    feat: instantiate database

---
+++ src/Creational/Singleton/DatabaseSettings/Program.cs
@@ -0,0 +1,8 @@
+﻿using DatabaseSettings;
+
+Database databaseInstance1 = Database.GetInstance();
+databaseInstance1.Query("SELECT * FROM Item");
+
+Database databaseInstance2 = Database.GetInstance();
+databaseInstance2.Query("SELECT * FROM Product");
+Console.WriteLine(ReferenceEquals(databaseInstance1, databaseInstance2));
