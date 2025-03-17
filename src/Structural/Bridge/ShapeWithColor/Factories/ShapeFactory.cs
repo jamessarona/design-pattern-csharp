42b1f8e0199ac6ebe0837e3f7fc9f11a0d4ca6d8
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 13:40:40 +0800

    feat: implement assigning of color and shape when creating shape

---
+++ src/Structural/Bridge/ShapeWithColor/Factories/ShapeFactory.cs
@@ -0,0 +1,19 @@
+﻿using Colors;
+using Shapes;
+
+namespace Factories;
+
+class ShapeFactory
+{
+    public static Shape CreateShape(string shapeType, string colorType)
+    {
+        IColor color = colorType == "Read" ? new Red() : new Blue();
+
+        return shapeType switch
+        {
+            "Circle" => new Circle(color),
+            "Square" => new Square(color),
+            _ => throw new ArgumentException("Invalid shape type")
+        };
+    }
+}
