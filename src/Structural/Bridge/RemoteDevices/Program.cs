bf7fe2526b211d93109d479ed771bb377be67a9f
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 15:11:35 +0800

    feat: add sample scenario

--- src/Structural/Bridge/RemoteDevices/Program.cs
+++ src/Structural/Bridge/RemoteDevices/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using RemoteDevices.Devices;
+using RemoteDevices.Remotes;
+
+IDevice tv = new TV();
+Remote remote = new Remote(tv);
+
+Console.WriteLine(tv);
+remote.TogglePower();
+remote.VolumeUp();
+remote.ChannelUp();
+Console.WriteLine(tv);
+
+IDevice radio = new Radio();
+AdvancedRemote advRemote = new AdvancedRemote(radio);
+
+Console.WriteLine(radio);
+advRemote.TogglePower();
+advRemote.Mute();
+advRemote.ChannelDown();
+Console.WriteLine(radio);
