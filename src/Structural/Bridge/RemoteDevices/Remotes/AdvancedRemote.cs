f471b04692683168e59cf9e10779cdf4416f886a
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 15:11:25 +0800

    feat: implement advanced remote with behavior

---
+++ src/Structural/Bridge/RemoteDevices/Remotes/AdvancedRemote.cs
@@ -0,0 +1,17 @@
+﻿using RemoteDevices.Devices;
+
+namespace RemoteDevices.Remotes;
+
+// Extended Abstraction: AdvancedRemote
+public class AdvancedRemote : Remote
+{
+    public AdvancedRemote(IDevice device) : base(device) 
+    {
+    
+    }
+
+    public void Mute()
+    {
+        _device.SetVolume(0);
+    }
+}
