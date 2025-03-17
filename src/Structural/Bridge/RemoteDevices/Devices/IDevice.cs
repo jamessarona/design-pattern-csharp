36ef6d7892002f5a57952fad270ea6f4f39a8dd1
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 15:10:08 +0800

    feat: add device contract/template

---
+++ src/Structural/Bridge/RemoteDevices/Devices/IDevice.cs
@@ -0,0 +1,13 @@
+﻿namespace RemoteDevices.Devices;
+
+// Device Interface (Implementation)
+public interface IDevice
+{
+    bool IsEnabled();
+    void Enable();
+    void Disable();
+    int GetVolume();
+    void SetVolume(int percentage);
+    int GetChannel();
+    void SetChannel(int channel);
+}
