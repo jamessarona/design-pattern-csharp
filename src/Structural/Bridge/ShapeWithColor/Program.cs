f60b4d26c896ddab3f87a5cc6df858cf88d0951e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 13:40:53 +0800

    feat: add sample implementation when creating shape

--- src/Structural/Bridge/ShapeWithColor/Program.cs
+++ src/Structural/Bridge/ShapeWithColor/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using Factories;
+using Shapes;
+
+Shape shape1 = ShapeFactory.CreateShape("Circle", "Blue");
+Console.WriteLine(shape1.Draw()); // Output: Drawing Circle in Blue
+
+Shape shape2 = ShapeFactory.CreateShape("Square", "Red");
+Console.WriteLine(shape2.Draw()); // Output: Drawing Square in Red
