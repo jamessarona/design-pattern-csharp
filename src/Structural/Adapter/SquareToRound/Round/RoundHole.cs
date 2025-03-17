2f34dac7c4c43c49a41b1d594b10bdecb2260f91
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 12:51:05 +0800

    feat: add round hole and peg class

---
+++ src/Structural/Adapter/SquareToRound/Round/RoundHole.cs
@@ -0,0 +1,21 @@
+﻿namespace SquareToRound.Round;
+
+public class RoundHole
+{
+    private double radius;
+
+    public RoundHole(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return radius;
+    }
+
+    public bool Fits(RoundPeg peg)
+    {
+        return peg.GetRadius() <= radius;
+    }
+}
---
+++ src/Structural/Adapter/SquareToRound/Round/RoundPeg.cs
@@ -0,0 +1,16 @@
+﻿namespace SquareToRound.Round;
+
+public class RoundPeg
+{
+    private double radius;
+
+    public RoundPeg(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return radius;
+    }
+}
