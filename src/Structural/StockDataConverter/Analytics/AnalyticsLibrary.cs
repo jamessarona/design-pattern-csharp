711cf74fb58768d34ecd920c40ba30a069d1695b
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 12:36:44 +0800

    feat: implement sample output

---
+++ src/Structural/StockDataConverter/Analytics/AnalyticsLibrary.cs
@@ -0,0 +1,11 @@
+﻿using StockDataConverter.Base;
+
+namespace StockDataConverter.Analytics;
+
+public class AnalyticsLibrary
+{
+    public void ProcessStockData(IStockData stockData)
+    {
+        Console.WriteLine("Processing Data: " + stockData.GetStockData());
+    }
+}
--- src/Structural/StockDataConverter/Program.cs
+++ src/Structural/StockDataConverter/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using StockDataConverter.Adapter;
+using StockDataConverter.Analytics;
+using StockDataConverter.Base;
+using StockDataConverter.Data;
+
+// XML data source
+StockDataXml xmlData = new StockDataXml();
+
+// Adapter to convert to XML to JSON
+IStockData adaptedData = new XmlToJsonAdapter(xmlData);
+
+// Analytics system that requires JSON
+AnalyticsLibrary analytics = new AnalyticsLibrary();
+analytics.ProcessStockData(adaptedData); // Process JSON instead of XML
+
+Console.ReadLine();
