4d9d2505f901f27eae0bdfd42d3ca0224fceb8c3
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 15:10:38 +0800

    feat: implement radio class inheriting remote

---
+++ src/Structural/Bridge/RemoteDevices/Devices/Radio.cs
@@ -0,0 +1,41 @@
+﻿namespace RemoteDevices.Devices;
+
+class Radio : IDevice
+{
+    private bool _isEnabled = false;
+    private int _volume = 30;
+    private int _channel = 90;
+
+    public bool IsEnabled()
+    {
+        return _isEnabled;
+    }
+    public void Enable()
+    {
+        _isEnabled = true;
+    }
+    public void Disable()
+    {
+        _isEnabled = false;
+    }
+    public int GetVolume()
+    {
+        return _volume;
+    }
+    public void SetVolume(int percentage)
+    {
+        _volume = percentage;
+    }
+    public int GetChannel()
+    {
+        return _channel;
+    }
+    public void SetChannel(int channel)
+    {
+        _channel = channel;
+    }
+    public override string ToString()
+    {
+        return $"Radio - Power: {_isEnabled}, Volume: {_volume}, Channel: {_channel}";
+    }
+}
