2f3b7a8f1f8e06f14acc12134f98b755cf3f18f6
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 15:10:46 +0800

    feat: implement TV class inheriting remote

---
+++ src/Structural/Bridge/RemoteDevices/Devices/TV.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Channels;
+
+namespace RemoteDevices.Devices;
+
+// Concrete Implementation: TV
+public class TV : IDevice
+{
+    private bool _isEnabled = false;
+    private int _volume = 50;
+    private int _channel = 1;
+
+    public bool IsEnabled()
+    {
+        return _isEnabled;
+    }
+
+    public void Enable()
+    {
+        _isEnabled = true;
+    }
+
+    public void Disable()
+    {
+        _isEnabled = false;
+    }
+
+    public int GetVolume()
+    {
+        return _volume;
+    }
+
+    public void SetVolume(int percentage)
+    {
+        _volume = percentage;
+    }
+
+    public int GetChannel()
+    {
+        return _channel;
+    }
+
+    public void SetChannel(int channel)
+    {
+        _channel = channel;
+    }
+
+    public override string ToString()
+    {
+        return $"TV - Power: {_isEnabled}, Volume: {_volume}, Channel: {_channel}";
+    }
+}
