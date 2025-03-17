c260c816f47db37d941fb16ebae5c8a3faed645e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-17 12:36:15 +0800

    feat: add xml serializer into json

---
+++ src/Structural/StockDataConverter/Adapter/XmlToJsonAdapter.cs
@@ -0,0 +1,27 @@
+﻿using StockDataConverter.Base;
+using StockDataConverter.Data;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace StockDataConverter.Adapter;
+
+public class XmlToJsonAdapter : IStockData
+{
+    private readonly StockDataXml _xmlStockData;
+
+    public XmlToJsonAdapter(StockDataXml xmlStockData)
+    {
+        _xmlStockData = xmlStockData;
+    }
+
+    public string GetStockData()
+    {
+        string xmlData = _xmlStockData.GetStockData();
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(xmlData);
+
+        string jsonData = JsonConvert.SerializeXmlNode(xmlDocument);
+        return jsonData;
+
+    }
+}
