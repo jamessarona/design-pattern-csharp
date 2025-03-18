03a4fa07edfe6cbccd67cf3c6f79b5f0f46bdced
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-18 13:16:19 +0800

    feat: implement Compound Graphic

---
+++ src/Structural/Composite/ImageEditor/CompoundGraphic.cs
@@ -0,0 +1,34 @@
+﻿namespace ImageEditorComposite;
+
+public class CompoundGraphic : IGraphic
+{
+    private readonly List<IGraphic> children = new List<IGraphic>();
+
+    public void Add(IGraphic child)
+    {
+        children.Add(child);
+    }
+
+    public void Remove(IGraphic child)
+    {
+       children.Remove(child);
+    }
+
+    public void Move(int x, int y)
+    {
+        Console.WriteLine($"Moving CompoundGraphic by ({x}, {y})");
+        foreach (var child in children)
+        {
+            child.Move(x, y);
+        }
+    }
+
+    public void Draw()
+    {
+        Console.WriteLine("Drawing CompoundGraphic:");
+        foreach (var child in  children)
+        {
+            child.Draw();
+        }
+    }
+}
