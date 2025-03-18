3466cf210987359bcc362cf12c81dc64c03624cd
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-18 12:41:11 +0800

    feat: add example output to test functionality of composite pattern

---
+++ src/Structural/Composite/SampleComposite/Program.cs
@@ -0,0 +1,13 @@
+﻿using SampleComposite;
+
+// Create leaf components
+IComponent leaf1 = new Leaf();
+IComponent leaf2 = new Leaf();
+
+// Create composite and add leaf components
+Composite composite = new Composite();
+composite.Add(leaf1);
+composite.Add(leaf2);
+
+// Execute the composite (which delegates to children)
+composite.Execute();
