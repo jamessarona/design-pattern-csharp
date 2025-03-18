a5d8db328f97d950557d5ed799fedb7f3bacef12
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-18 13:15:34 +0800

    feat: implement Dot class implementing IGraphic

---
+++ src/Structural/Composite/ImageEditor/Dot.cs
@@ -0,0 +1,25 @@
+﻿namespace ImageEditorComposite;
+
+// Leaf Class (Single Graphic Object)
+public class Dot : IGraphic
+{
+    protected int X, Y;
+
+    public Dot(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Move(int x, int y)
+    {
+        X += x;
+        Y += y;
+        Console.WriteLine($"Dot moved to ({X}, {Y})");
+    }
+
+    public void Draw()
+    {
+        Console.WriteLine($"Drawing a dot at ({X}, {Y})");
+    }
+}
