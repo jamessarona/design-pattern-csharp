ee636818f87e730bd0a14dbd382a22b5e59d9a7c
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-18 13:16:01 +0800

    feat: add Circle class inheriting Dot class

---
+++ src/Structural/Composite/ImageEditor/Circle.cs
@@ -0,0 +1,16 @@
+﻿namespace ImageEditorComposite;
+
+public class Circle : Dot
+{
+    private int Radius;
+
+    public Circle(int x, int y, int radius) : base(x, y)
+    {
+        Radius = radius;
+    }
+
+    public new void Draw()
+    {
+        Console.WriteLine($"Drawing a circle at ({X}, {Y}) with radius {Radius}");
+    }
+}
