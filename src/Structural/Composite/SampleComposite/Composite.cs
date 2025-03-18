e786bfe1974a8cec0b01d1435ed5d617959c0d3a
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-18 12:40:28 +0800

    feat: implement composite behavior

---
+++ src/Structural/Composite/SampleComposite/Composite.cs
@@ -0,0 +1,31 @@
+﻿namespace SampleComposite;
+
+// Composite Class
+public class Composite : IComponent
+{
+    private readonly List<IComponent> _children = new List<IComponent>();
+
+    public void Add(IComponent component)
+    {
+        _children.Add(component);
+    }
+
+    public void Remove(IComponent component)
+    {
+        _children.Remove(component);
+    }
+
+    public List<IComponent> GetChildren()
+    {
+        return _children;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine("Composite: Delegating work to children.");
+        foreach (var component in _children)
+        {
+            component.Execute();
+        }
+    }
+}
