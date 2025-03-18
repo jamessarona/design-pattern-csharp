2f4e126b50656cb6e623834870be56018754754c
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-18 13:16:39 +0800

    feat: add example for image editor

---
+++ src/Structural/Composite/ImageEditor/ImageEditor.cs
@@ -0,0 +1,34 @@
+﻿namespace ImageEditorComposite;
+
+// Client Class
+public class ImageEditor
+{
+    private CompoundGraphic _allGraphics = new CompoundGraphic();
+
+    public void Load()
+    {
+        // Adding individual graphics (Dot and Circle)
+        _allGraphics.Add(new Dot(1, 2));
+        _allGraphics.Add(new Circle(5, 5, 3));
+
+        // Creating a nested compound graphic
+        CompoundGraphic group = new CompoundGraphic();
+        group.Add(new Dot(10, 10));
+        group.Add(new Circle(15, 15, 5));
+
+        // Adding the nested group to the main compound graphic
+        _allGraphics.Add(group);
+    }
+
+    public void Move(int x, int y)
+    {
+        Console.WriteLine($"Moving all graphics by ({x}, {y})");
+        _allGraphics.Move(x, y);
+    }
+
+    public void Draw()
+    {
+        Console.WriteLine("Rendering all graphics:");
+        _allGraphics.Draw();
+    }
+}
--- src/Structural/Composite/ImageEditor/Program.cs
+++ src/Structural/Composite/ImageEditor/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using ImageEditorComposite;
+
+ImageEditor editor = new ImageEditor();
+
+// Load objects
+editor.Load();
+
+// Draw all graphics
+editor.Draw();
+
+// Move graphics
+editor.Move(2, 3);
+
+// Redraw after moving
+editor.Draw();
