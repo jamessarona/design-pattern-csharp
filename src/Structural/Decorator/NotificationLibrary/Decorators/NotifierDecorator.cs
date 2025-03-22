a564c0dbe6808e552e2c77fe625beb839a457e65
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-22 10:41:04 +0800

    feat: implement notifier decorator

---
+++ src/Structural/Decorator/NotificationLibrary/Decorators/NotifierDecorator.cs
@@ -0,0 +1,18 @@
+﻿using NotificationLibrary.Base;
+
+namespace NotificationLibrary.Decorators;
+
+public abstract class NotifierDecorator : INotifier
+{
+    protected INotifier _notifier;
+
+    public NotifierDecorator(INotifier notifier)
+    {
+        _notifier = notifier;
+    }
+
+    public virtual void Send(string message)
+    {
+        _notifier.Send(message);
+    }
+}
