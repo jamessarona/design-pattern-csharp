879394507b6321b35247eed336c449a46c4e6544
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-22 10:42:01 +0800

    feat: add sending of notification

---
+++ src/Structural/Decorator/NotificationLibrary/Program.cs
@@ -0,0 +1,17 @@
+﻿using NotificationLibrary.Base;
+using NotificationLibrary.Decorators;
+
+// Base notifier
+INotifier notifier = new Notifier();
+
+// Adding Email Notifier
+notifier = new EmailNotifier(notifier);
+
+// Adding SMS Notifier
+notifier = new SMSNotifier(notifier);
+
+// Adding Slack Notifier
+notifier = new SlackNotifier(notifier);
+
+// Sending Notification
+notifier.Send("Alert!");
