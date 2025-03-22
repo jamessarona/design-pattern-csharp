f28bf6a1c06dddd3b097c864d38786181dc901bd
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-22 10:41:32 +0800

    feat: implement SMS Notifier

---
+++ src/Structural/Decorator/NotificationLibrary/Decorators/SmsNotifier.cs
@@ -0,0 +1,14 @@
+﻿using NotificationLibrary.Base;
+
+namespace NotificationLibrary.Decorators;
+
+public class SMSNotifier : NotifierDecorator
+{
+    public SMSNotifier(INotifier notifier) : base(notifier) { }
+
+    public override void Send(string message)
+    {
+        base.Send(message);
+        Console.WriteLine($"SMS Notification Sent: {message}");
+    }
+}
