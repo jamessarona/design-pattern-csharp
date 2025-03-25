042ccfd5b0d8192b9b97c051baf906913542c140
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 16:32:14 +0800

    feat: add implementation of configurator

---
+++ src/Structural/Decorator/DataEncrypter/Client/ApplicationConfigurator.cs
@@ -0,0 +1,35 @@
+﻿using DataEncrypter.Base;
+using DataEncrypter.DataSource;
+using DataEncrypter.Decorators;
+
+namespace DataEncrypter.Client;
+
+public class ApplicationConfigurator
+{
+    public static void ConfigurationExample(bool enableEncryption, bool enableCompression)
+    {
+        IDataSource source = new FileDataSource("salary.dat");
+
+        if (enableCompression)
+        {
+            source = new CompressionDecorator(source);
+        }
+        if (enableEncryption)
+        {
+            source = new EncryptionDecorator(source);
+        }
+
+        SalaryManager manager = new SalaryManager(source);
+
+        string salaryRecords = "EmployeeSalary1234";
+        Console.WriteLine("Original Data: " + salaryRecords);
+
+        // Save Data
+        manager.Save(salaryRecords);
+        Console.WriteLine("Data saved to file.");
+
+        // Load Data
+        string loadedData = manager.Load();
+        Console.WriteLine("Loaded Data: " + loadedData);
+    }
+}
