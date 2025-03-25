1487f31c48719b41133cf00d8b7d3d571dbbb370
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 17:00:17 +0800

    feat: implement video converter emphasizing facade

---
+++ src/Structural/Facade/VideoConverterProject/Facade/VideoConverter.cs
@@ -0,0 +1,25 @@
+﻿using VideoConverterProject.SubSystems;
+
+namespace VideoConverterProject.Facade;
+
+public class VideoConverter
+{
+    public void ConvertVideo(string fileName, string targetFormat)
+    {
+        Console.WriteLine($"Starting video conversion: {fileName} -> {targetFormat}");
+
+        VideoFile file = BitrateReader.Read(fileName);
+        ICodec sourceCodec = CodecFactory.Extract(file);
+
+        ICodec targetCodec = targetFormat.ToLower() == "ogg" ? new OggCompressionCodec() : new Mpeg4CompressionCodec();
+
+        Console.WriteLine($"Converting {file.FileName} from {sourceCodec.CodecName} to {targetCodec.CodecName}");
+
+        BitrateReader.Write(file);
+
+        AudioMixer audioMixer = new AudioMixer();
+        audioMixer.FixAudio(file);
+
+        Console.WriteLine($"Conversion completed: {file.FileName}.{targetFormat}");
+    }
+}
