9667cc2a4bd83fedbc330d493c590abc9ee09564
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 16:30:36 +0800

    feat: implement file data source referencing to IDataSource

---
+++ src/Structural/Decorator/DataEncrypter/DataSource/FileDataSource.cs
@@ -0,0 +1,22 @@
+﻿using DataEncrypter.Base;
+
+namespace DataEncrypter.DataSource;
+
+public class FileDataSource : IDataSource
+{
+    private string filename;
+    
+    public FileDataSource(string filename)
+    {
+        this.filename = filename;
+    }
+    public void WriteData(string data)
+    {
+        File.WriteAllText(filename, data);
+    }
+
+    public string ReadData()
+    {
+        return File.Exists(filename) ? File.ReadAllText(filename) : string.Empty;
+    }
+}
