1b9d48358a0d0c4ae6a0e402bd2aa6a5da155d02
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 16:31:16 +0800

    feat: add compression of file

---
+++ src/Structural/Decorator/DataEncrypter/Decorators/CompressionDecorator.cs
@@ -0,0 +1,48 @@
+﻿using DataEncrypter.Base;
+using DataEncrypter.Decorators;
+using System.IO.Compression;
+using System.Text;
+
+public class CompressionDecorator : DataSourceDecorator
+{
+    public CompressionDecorator(IDataSource wrappee) : base(wrappee) { }
+
+    public override void WriteData(string data)
+    {
+        string compressedData = Compress(data);
+        base.WriteData(compressedData);
+    }
+
+    public override string ReadData()
+    {
+        string compressedData = base.ReadData();
+        return Decompress(compressedData);
+    }
+
+    private string Compress(string data)
+    {
+        byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+        using (var outputStream = new MemoryStream())
+        {
+            using (var gzipStream = new GZipStream(outputStream, CompressionMode.Compress))
+            {
+                gzipStream.Write(dataBytes, 0, dataBytes.Length);
+            }
+            return Convert.ToBase64String(outputStream.ToArray()); // Store as Base64
+        }
+    }
+
+    private string Decompress(string compressedData)
+    {
+        byte[] compressedBytes = Convert.FromBase64String(compressedData);
+
+        using (var inputStream = new MemoryStream(compressedBytes))
+        using (var gzipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+        using (var outputStream = new MemoryStream())
+        {
+            gzipStream.CopyTo(outputStream);
+            return Encoding.UTF8.GetString(outputStream.ToArray());
+        }
+    }
+}
