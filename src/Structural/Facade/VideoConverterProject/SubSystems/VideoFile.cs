793fe19df56f0535ad8797b383fd863a1b45329f
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 16:59:57 +0800

    feat: implement video file subclass

---
+++ src/Structural/Facade/VideoConverterProject/SubSystems/VideoFile.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoConverterProject.SubSystems;
+
+public class VideoFile
+{
+    public string FileName { get; }
+    public string Format { get; }
+
+    public VideoFile(string fileName)
+    {
+        FileName = fileName;
+        Format = fileName.Substring(fileName.LastIndexOf('.') + 1);
+    }
+}
