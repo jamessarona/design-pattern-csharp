603df72df6b62f20a7c534183029d129add44518
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 16:30:58 +0800

    feat: implement Data Source Decorator

---
+++ src/Structural/Decorator/DataEncrypter/Decorators/DataSourceDecorator.cs
@@ -0,0 +1,23 @@
+﻿using DataEncrypter.Base;
+
+namespace DataEncrypter.Decorators;
+
+public class DataSourceDecorator : IDataSource
+{
+    private IDataSource wrappee;
+
+    public DataSourceDecorator(IDataSource wrappee)
+    {
+        this.wrappee = wrappee;
+    }
+
+    public virtual void WriteData(string data)
+    {
+        wrappee.WriteData(data);
+    }
+
+    public virtual string ReadData()
+    {
+        return wrappee.ReadData();
+    }
+}
