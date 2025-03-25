4203d8c0d2bc24f05f335cfa20073cf00dc611f0
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 16:58:57 +0800

    feat: implement bitrate reader subclass

---
+++ src/Structural/Facade/VideoConverterProject/SubSystems/BitrateReader.cs
@@ -0,0 +1,15 @@
+﻿namespace VideoConverterProject.SubSystems;
+
+public static class BitrateReader
+{
+    public static VideoFile Read(string fileName)
+    {
+        Console.WriteLine($"Reading bitrate from {fileName}");
+        return new VideoFile(fileName);
+    }
+
+    public static void Write(VideoFile file)
+    {
+        Console.WriteLine($"Writing bitrate for {file.FileName}");
+    }
+}
