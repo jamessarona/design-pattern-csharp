0e5b3a046020d3ffadd4d14da1fb63653dad3ddf
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 16:58:46 +0800

    feat: implement audio mixer subclass

---
+++ src/Structural/Facade/VideoConverterProject/SubSystems/AudioMixer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace VideoConverterProject.SubSystems;
+
+public class AudioMixer
+{
+    public void FixAudio(VideoFile file)
+    {
+        Console.WriteLine($"Fixing audio for {file.FileName}");
+    }
+}
