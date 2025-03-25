babe23b6044f623000e480f4ed46283dd2dd7c9e
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-25 17:00:25 +0800

    feat: add sample

---
+++ src/Structural/Facade/VideoConverterProject/Program.cs
@@ -0,0 +1,9 @@
+﻿using VideoConverterProject.Facade;
+
+VideoConverter converter = new VideoConverter();
+
+Console.WriteLine("Converting to MPEG4...");
+converter.ConvertVideo("example.ogg", "mp4");
+
+Console.WriteLine("\nConverting to OGG...");
+converter.ConvertVideo("example.mp4", "ogg");
