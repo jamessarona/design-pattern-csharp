235bc83831fd2c693ab1fc48326f307738144782
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-26 11:07:50 +0800

    feat: add Tree Factory for adding unique tree type

---
+++ src/Structural/Flyweight/TreeObjectLibrary/Factories/TreeFactory.cs
@@ -0,0 +1,25 @@
+﻿using TreeObjectLibrary.Models;
+
+namespace TreeObjectLibrary.Factories;
+
+public class TreeFactory
+{
+    private static Dictionary<string, TreeType> TreeTypes = new Dictionary<string, TreeType>();
+    
+    public static TreeType GetTreeType(string Name, string Color, string Texture)
+    {
+        string key = $"{Name}_{Color}_{Texture}";
+
+        if (!TreeTypes.ContainsKey(key))
+        {
+            TreeTypes[key] = new TreeType(Name, Color, Texture);
+            Console.WriteLine($"Created new TreeType: {Name}, Color: {Color}, Texture: {Texture}");
+        }
+        else
+        {
+            Console.WriteLine($"Reusing existing TreeType: {Name}, Color: {Color}, Texture: {Texture}");
+        }
+
+        return TreeTypes[key];
+    }
+}
