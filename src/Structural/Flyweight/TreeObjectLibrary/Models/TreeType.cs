00fc6ea157cb659b56bf8caba1314b8cd62fed22
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-26 11:06:51 +0800

    feat: add Tree Type model

---
+++ src/Structural/Flyweight/TreeObjectLibrary/Models/TreeType.cs
@@ -0,0 +1,20 @@
+﻿namespace TreeObjectLibrary.Models;
+
+public class TreeType
+{
+    public string Name { get; }
+    public string Color { get; }
+    public string Texture { get; }
+
+    public TreeType(string Name, string Color, string Texture)
+    {
+        this.Name = Name;
+        this.Color = Color;
+        this.Texture = Texture;
+    }
+
+    public void Draw(int x, int y)
+    {
+        Console.WriteLine($"Drawing tree '{Name}' at ({x}, {y}) with color {Color} and texture {Texture}.");
+    }
+}
