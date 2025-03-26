d1d74d1b3f9e192a7dd9d9a3318d1ecdf763b74b
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-26 11:08:20 +0800

    feat: add sample scenario

---
+++ src/Structural/Flyweight/TreeObjectLibrary/Program.cs
@@ -0,0 +1,9 @@
+﻿using TreeObjectLibrary.Client;
+
+Forest forest = new Forest();
+
+forest.PlantTree(10, 20, "Oak", "Green", "Rough Bark");
+forest.PlantTree(30, 40, "Pine", "Dark Green", "Smooth Bark");
+forest.PlantTree(50, 60, "Oak", "Green", "Rough Bark"); // Reuses existing "Oak" type
+
+forest.Draw("MyCanvas");
