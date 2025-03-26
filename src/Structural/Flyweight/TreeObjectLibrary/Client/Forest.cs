f3b73ddf65d68ff3f342815177f2ac69f9130407
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-26 11:08:10 +0800

    feat: add Forest to add Tree and Draw tree

---
+++ src/Structural/Flyweight/TreeObjectLibrary/Client/Forest.cs
@@ -0,0 +1,25 @@
+﻿using TreeObjectLibrary.Factories;
+using TreeObjectLibrary.Models;
+
+namespace TreeObjectLibrary.Client;
+
+public class Forest
+{
+    public List<Tree> Trees = new List<Tree>();
+
+    public void PlantTree(int X, int Y, string Name, string Color, string Texture) 
+    {
+        TreeType Type = TreeFactory.GetTreeType(Name, Color, Texture);
+        Tree Tree = new Tree(X, Y, Type);
+        Trees.Add(Tree);
+    }
+
+    public void Draw(string Canvas)
+    {
+        Console.WriteLine("Forest is " + Canvas);
+        foreach (var Tree in Trees)
+        {
+            Tree.Draw();
+        }
+    }
+}
