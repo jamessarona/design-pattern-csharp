82ff7af0a129da4d37d19cd86c4b416c51585a57
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-26 12:50:08 +0800

    feat: add common interface for third party youtube library

---
+++ src/Structural/Proxy/YoutubeManager/IThirdPartyYoutubeLib.cs
@@ -0,0 +1,11 @@
+﻿using YoutubeManager.Models;
+using YoutubeManager.Services;
+
+namespace YoutubeManager;
+
+public interface IThirdPartyYoutubeLib
+{
+    List<Video> ListVideos();
+    Video GetVideoInfo(string id);
+    void DownloadVideo(string id);
+}
