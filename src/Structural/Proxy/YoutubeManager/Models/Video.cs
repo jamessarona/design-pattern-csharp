0433d1f09aab784cbd6320a03c454314c38fa51d
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-26 12:50:20 +0800

    feat: add video model

---
+++ src/Structural/Proxy/YoutubeManager/Models/Video.cs
@@ -0,0 +1,17 @@
+﻿namespace YoutubeManager.Models;
+
+public class Video
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+
+    public Video(string id, string title, string description, string url)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Url = url;
+    }
+}
