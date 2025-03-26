a91542d1fab4dec5fb331f52e67a1ff703a610d3
James Angelo Sarona <jacsarona.official@gmail.com>
2025-03-26 11:07:05 +0800

    feat: add Tree model

---
+++ src/Structural/Flyweight/TreeObjectLibrary/Models/Tree.cs
@@ -0,0 +1,20 @@
+﻿namespace TreeObjectLibrary.Models;
+
+public class Tree
+{
+    public int X { get; }
+    public int Y { get; }
+    public TreeType Type { get; }
+
+    public Tree(int X, int Y, TreeType Type)
+    {
+        this.X = X;
+        this.Y = Y;
+        this.Type = Type;
+    }
+
+    public void Draw()
+    {
+        Type.Draw(X, Y);
+    }
+}
