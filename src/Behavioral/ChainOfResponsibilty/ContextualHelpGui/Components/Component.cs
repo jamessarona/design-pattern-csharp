3abd34237d09ccdaffd4b37159befde0d4b791ea
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-01 20:23:17 +0800

    feat: implement component

---
+++ src/Behavioral/ChainOfResponsibilty/ContextualHelpGui/Components/Component.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Xml.Linq;
+
+namespace ContextualHelpGui.Components;
+
+public abstract class Component : IComponentWithContextualHelp
+{
+    protected Container container { get; private set; }
+    protected string tooltipText;
+
+    public Component(Container container, string tooltipText)
+    {
+        this.container = container;
+        this.tooltipText = tooltipText;
+    }
+    protected void SetContainer(Container newContainer)
+    {
+        container = newContainer;
+    }
+
+    public void ShowHelp()
+    {
+        if (!string.IsNullOrEmpty(tooltipText))
+        {
+            Console.WriteLine($"Tooltip: {tooltipText}");
+        }
+        else if (container != null)
+        {
+            container.ShowHelp();
+        }
+    }
+}
