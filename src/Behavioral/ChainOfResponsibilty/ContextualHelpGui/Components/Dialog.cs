eb7a192ec52272b0b9360504121e3ceab89814ed
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-03 09:59:04 +0800

    feat: add dialog component

---
+++ src/Behavioral/ChainOfResponsibilty/ContextualHelpGui/Components/Dialog.cs
@@ -0,0 +1,23 @@
+﻿namespace ContextualHelpGui.Components;
+
+public class Dialog : Container
+{
+    private readonly string? wikiPageUrl;
+
+    public Dialog(string? wikiPageUrl = null)
+    {
+        this.wikiPageUrl = wikiPageUrl;
+    }
+
+    public override void ShowHelp()
+    {
+        if (!string.IsNullOrEmpty(wikiPageUrl))
+        {
+            Console.WriteLine($"Opening Wiki Page: {wikiPageUrl}");
+        }
+        else
+        {
+            base.ShowHelp();
+        }
+    }
+}
