3e9ebbe87b312ea884a47529ff101d5c2dfa8174
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-03 09:59:13 +0800

    feat: add panel component

---
+++ src/Behavioral/ChainOfResponsibilty/ContextualHelpGui/Components/Panel.cs
@@ -0,0 +1,23 @@
+﻿namespace ContextualHelpGui.Components;
+
+public class Panel : Container
+{
+    private readonly string? modalHelpText;
+
+    public Panel(string? modalHelpText = null)
+    {
+        this.modalHelpText = modalHelpText;
+    }
+
+    public override void ShowHelp()
+    {
+        if (!string.IsNullOrEmpty(modalHelpText))
+        {
+            Console.WriteLine($"Modal Help: {modalHelpText}");
+        }
+        else
+        {
+            base.ShowHelp();
+        }
+    }
+}
