f1208109986cdfe3aeb8d75411350bd33ff533ae
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:43:03 +0800

    feat: add application class

---
+++ src/Behavioral/Command/TextEditor/Core/Application.cs
@@ -0,0 +1,30 @@
+﻿using TextEditor.Commands;
+
+namespace TextEditor.Core;
+
+public class Application
+{
+    public List<Editor> Editors { get; set; } = new();
+    public Editor? ActiveEditor { get; set; }
+    public string? Clipboard { get; set; }
+    public CommandHistory History { get; set; } = new();
+
+    public void ExecuteCommand(Command command)
+    {
+        if (command.Execute())
+        {
+            History.Push(command);
+        }
+    }
+
+    public void Undo()
+    {
+        var command = History.Pop();
+        command?.Undo();
+    }
+
+    public void CreateUI()
+    {
+        // Simulate UI
+    }
+}
