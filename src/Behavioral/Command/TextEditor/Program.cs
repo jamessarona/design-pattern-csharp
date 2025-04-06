5bf741cb42ffc9aa6c475e2a83797d2a6da54aee
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:54:29 +0800

    feat: add sample scenario of command patter in Text Editor

--- src/Behavioral/Command/TextEditor/Program.cs
+++ src/Behavioral/Command/TextEditor/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TextEditor.Commands;
+using TextEditor.Core;
+
+var app = new Application();
+var editor = new Editor();
+app.ActiveEditor = editor;
+app.Editors.Add(editor);
+
+editor.Text = "Hello World";
+
+app.ExecuteCommand(new CopyCommand(app, editor));
+app.ExecuteCommand(new CutCommand(app, editor));
+app.ExecuteCommand(new PasteCommand(app, editor));
+app.ExecuteCommand(new UndoCommand(app, editor));
+
+Console.WriteLine("Editor text: " + editor.Text);
