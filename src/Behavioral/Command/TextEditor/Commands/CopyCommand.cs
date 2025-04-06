ac49ba03d51c71501758e7c843ec140deff4d05d
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:53:04 +0800

    feat: add copy command

---
+++ src/Behavioral/Command/TextEditor/Commands/CopyCommand.cs
@@ -0,0 +1,14 @@
+﻿using TextEditor.Core;
+
+namespace TextEditor.Commands;
+
+public class CopyCommand : Command
+{
+    public CopyCommand(Application app, Editor editor) : base(app, editor) { }
+
+    public override bool Execute()
+    {
+        App.Clipboard = Editor.GetSelection();
+        return false; // No need to undo copy
+    }
+}
