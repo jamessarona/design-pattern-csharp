54c1d9823752eb32b7584038740dbe240e138aa0
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:43:36 +0800

    feat: add command abstract class

---
+++ src/Behavioral/Command/TextEditor/Commands/Command.cs
@@ -0,0 +1,22 @@
+﻿using TextEditor.Core;
+
+namespace TextEditor.Commands;
+
+public abstract class Command
+{
+    private Application App;
+    private Editor Editor;
+    private string Backup;
+
+    public Command(Application app, Editor editor)
+    {
+        App = app;
+        Editor = editor;
+    }
+
+    protected void SaveBackup() => Backup = Editor.Text;
+
+    public virtual void Undo() => Editor.Text = Backup;
+
+    public abstract bool Execute();
+}
