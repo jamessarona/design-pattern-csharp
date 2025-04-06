6525d15a55557eaf9b38237fedbc16b9dc17f59a
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:43:16 +0800

    feat: add command history

---
+++ src/Behavioral/Command/TextEditor/Core/CommandHistory.cs
@@ -0,0 +1,12 @@
+﻿using TextEditor.Commands;
+
+namespace TextEditor.Core;
+
+public class CommandHistory
+{
+    public Stack<Command> history = new();
+
+    public void Push(Command command) => history.Push(command);
+
+    public Command? Pop() => history.Count > 0 ? history.Pop() : null;
+}
