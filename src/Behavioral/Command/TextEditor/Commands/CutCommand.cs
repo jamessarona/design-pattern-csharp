9c2e4f5dc1ba2519bfe63a288743b88cf04e53ca
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:53:13 +0800

    feat: add cut command

---
+++ src/Behavioral/Command/TextEditor/Commands/CutCommand.cs
@@ -0,0 +1,16 @@
+﻿using TextEditor.Core;
+
+namespace TextEditor.Commands;
+
+public class CutCommand : Command
+{
+    public CutCommand(Application app, Editor editor) : base(app, editor) { }
+
+    public override bool Execute()
+    {
+        SaveBackup();
+        App.Clipboard = Editor.GetSelection();
+        Editor.DeleteSelection();
+        return true;
+    }
+}
