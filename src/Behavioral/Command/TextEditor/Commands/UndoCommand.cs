9ff71bc11c6011a30ac79f13df9a9109d770af3a
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:53:34 +0800

    feat: add undo command class

---
+++ src/Behavioral/Command/TextEditor/Commands/UndoCommand.cs
@@ -0,0 +1,14 @@
+﻿using TextEditor.Core;
+
+namespace TextEditor.Commands;
+
+public class UndoCommand : Command
+{
+    public UndoCommand(Application app, Editor editor) : base(app, editor) { }
+
+    public override bool Execute()
+    {
+        App.Undo();
+        return false;
+    }
+}
