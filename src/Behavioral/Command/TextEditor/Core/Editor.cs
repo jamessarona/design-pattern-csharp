393010c7d8d08ae17a353e15625f63c201c18dc9
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:33:57 +0800

    feat: add editor class

---
+++ src/Behavioral/Command/TextEditor/Core/Editor.cs
@@ -0,0 +1,11 @@
+﻿namespace TextEditor.Core;
+
+public class Editor
+{
+    public string Text { get; set; } = default!;
+
+    public string GetSelection() => Text;
+    public void DeleteSelection() => Text = string.Empty;
+
+    public void ReplaceSelection(string text) => Text = text;
+}
