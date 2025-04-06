a04264743f955d284f3bdaf3a1557b703f2352c3
James Angelo Sarona <jacsarona.official@gmail.com>
2025-04-07 07:53:24 +0800

    feat: paste command class

---
+++ src/Behavioral/Command/TextEditor/Commands/PasteCommand.cs
@@ -0,0 +1,14 @@
+﻿using TextEditor.Core;
+
+namespace TextEditor.Commands;
+public class PasteCommand : Command
+{
+    public PasteCommand(Application app, Editor editor) : base(app, editor) { }
+
+    public override bool Execute()
+    {
+        SaveBackup();
+        Editor.ReplaceSelection(App.Clipboard);
+        return true;
+    }
+}
